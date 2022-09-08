11ed7c5045e9fddafa5aafc9b4d78194e5acc70a
donosonaumczuk <adonoso@itba.edu.ar>
2022-09-08 17:39:24 +0100

    feat: DegreesOfSeparationReferenceModule implementation improved

--- contracts/reference/DegreesOfSeparationReferenceModule.sol
+++ contracts/reference/DegreesOfSeparationReferenceModule.sol
@@ -12,25 +12,50 @@ import {IReferenceModule} from '@aave/lens-protocol/contracts/interfaces/IRefere
 import {ModuleBase} from '@aave/lens-protocol/contracts/core/modules/ModuleBase.sol';
 
 /**
+ * @notice Struct representing the module configuration for certain publication.
+ *
+ * @param setUp Indicates if the publication was set up to use this module, to then allow updating params.
+ * @param commentsRestricted Indicates if the comment operation is restricted or open to everyone.
+ * @param mirrorsRestricted Indicates if the mirror operation is restricted or open to everyone.
+ * @param degreesOfSeparation The max degrees of separation allowed for restricted operations.
+ */
+struct ModuleConfig {
+    bool setUp;
+    bool commentsRestricted;
+    bool mirrorsRestricted;
+    uint8 degreesOfSeparation;
+}
+
+/**
  * @title DegreesOfSeparationReferenceModule
  * @author Lens Protocol
  *
- * @notice This reference module allows to set a degree of separation N, and then allows to comment only to profiles
- * that are at most at N degrees of separation from the author of the root publication.
+ * @notice This reference module allows to set a degree of separation N, and then allows to comment/mirror only to
+ * profiles that are at most at N degrees of separation from the author of the root publication.
  */
 contract DegreesOfSeparationReferenceModule is ModuleBase, IReferenceModule {
-    event DegreesOfSeparationSet(
+    event ModuleParametersUpdated(
         uint256 indexed profileId,
         uint256 indexed pubId,
+        bool commentsRestricted,
+        bool mirrorsRestricted,
         uint8 degreesOfSeparation
     );
 
     error InvalidDegreesOfSeparation();
-    error CommentsDisabled();
+    error OpereationDisabled();
     error InvalidProfilePathLength();
+    error PublicationNotSetUp();
 
-    uint8 public immutable MAX_DEGREES_OF_SEPARATION = 6;
-    mapping(uint256 => mapping(uint256 => uint8)) internal _degreesOfSeparationByPubByProfile;
+    /**
+     * @dev Because of the "Six degrees of separation" theory, in the long term, setting up 5, 6 or more degrees of
+     * separation will be almost equivalent to turning off the restriction.
+     * If we also take into account the gas cost of performing the validations on-chain, makes sense to only support up
+     * to 4 degrees of separation.
+     */
+    uint8 constant MAX_DEGREES_OF_SEPARATION = 4;
+
+    mapping(uint256 => mapping(uint256 => ModuleConfig)) internal _moduleConfigByPubByProfile;
 
     constructor(address hub) ModuleBase(hub) {}
 
@@ -49,17 +74,125 @@ contract DegreesOfSeparationReferenceModule is ModuleBase, IReferenceModule {
         uint256 pubId,
         bytes calldata data
     ) external override returns (bytes memory) {
-        uint8 degreesOfSeparation = abi.decode(data, (uint8));
+        (bool commentsRestricted, bool mirrorsRestricted, uint8 degreesOfSeparation) = abi.decode(
+            data,
+            (bool, bool, uint8)
+        );
         if (degreesOfSeparation > MAX_DEGREES_OF_SEPARATION) {
             revert InvalidDegreesOfSeparation();
         }
-        _degreesOfSeparationByPubByProfile[profileId][pubId] = degreesOfSeparation;
+        _moduleConfigByPubByProfile[profileId][pubId] = ModuleConfig(
+            true,
+            commentsRestricted,
+            mirrorsRestricted,
+            degreesOfSeparation
+        );
         return data;
     }
 
     /**
      * @notice Processes a comment action referencing a given publication. This can only be called by the hub.
      *
+     * @dev It will apply the degrees of separation restriction if the publication has `commentsRestricted` enabled.
+     *
+     * @param profileId The token ID of the profile associated with the publication being published.
+     * @param profileIdPointed The profile ID of the profile associated the publication being referenced.
+     * @param pubIdPointed The publication ID of the publication being referenced.
+     * @param data Encoded data containing the array of profile IDs representing the follower path between the owner of
+     * the author of the root publication and the profile authoring the comment.
+     */
+    function processComment(
+        uint256 profileId,
+        uint256 profileIdPointed,
+        uint256 pubIdPointed,
+        bytes calldata data
+    ) external view override {
+        if (_moduleConfigByPubByProfile[profileIdPointed][pubIdPointed].commentsRestricted) {
+            _validateDegreesOfSeparationRestriction(
+                profileId,
+                profileIdPointed,
+                _moduleConfigByPubByProfile[profileIdPointed][pubIdPointed].degreesOfSeparation,
+                abi.decode(data, (uint256[]))
+            );
+        }
+    }
+
+    /**
+     * @notice Processes a mirror action referencing a given publication. This can only be called by the hub.
+     *
+     * @dev It will apply the degrees of separation restriction if the publication has `mirrorsRestricted` enabled.
+     *
+     * @param profileId The token ID of the profile associated with the publication being published.
+     * @param profileIdPointed The profile ID of the profile associated the publication being referenced.
+     * @param pubIdPointed The publication ID of the publication being referenced.
+     * @param data Encoded data containing the array of profile IDs representing the follower path between the owner of
+     * the author of the root publication and the profile authoring the mirror.
+     */
+    function processMirror(
+        uint256 profileId,
+        uint256 profileIdPointed,
+        uint256 pubIdPointed,
+        bytes calldata data
+    ) external view override {
+        if (_moduleConfigByPubByProfile[profileIdPointed][pubIdPointed].mirrorsRestricted) {
+            _validateDegreesOfSeparationRestriction(
+                profileId,
+                profileIdPointed,
+                _moduleConfigByPubByProfile[profileIdPointed][pubIdPointed].degreesOfSeparation,
+                abi.decode(data, (uint256[]))
+            );
+        }
+    }
+
+    /**
+     * @notice Updates the module parameters for the given publication.
+     *
+     * @param profileId The token ID of the profile publishing the publication.
+     * @param pubId The associated publication's LensHub publication ID.
+     * @param commentsRestricted Indicates if the comment operation is restricted or open to everyone.
+     * @param mirrorsRestricted Indicates if the mirror operation is restricted or open to everyone.
+     * @param degreesOfSeparation The max degrees of separation allowed for restricted operations.
+     */
+    function updateModuleParameters(
+        uint256 profileId,
+        uint256 pubId,
+        bool commentsRestricted,
+        bool mirrorsRestricted,
+        uint8 degreesOfSeparation
+    ) external {
+        if (!_moduleConfigByPubByProfile[profileId][pubId].setUp) {
+            revert PublicationNotSetUp();
+        }
+        if (degreesOfSeparation > MAX_DEGREES_OF_SEPARATION) {
+            revert InvalidDegreesOfSeparation();
+        }
+        _moduleConfigByPubByProfile[profileId][pubId].degreesOfSeparation = degreesOfSeparation;
+        emit ModuleParametersUpdated(
+            profileId,
+            pubId,
+            commentsRestricted,
+            mirrorsRestricted,
+            degreesOfSeparation
+        );
+    }
+
+    /**
+     * @notice Gets the degrees of separation set for the given publication.
+     *
+     * @param profileId The token ID of the profile publishing the publication.
+     * @param pubId The associated publication's LensHub publication ID.
+     *
+     * @return uint8 The degrees of separation set for the given publication.
+     */
+    function getDegreesOfSeparation(uint256 profileId, uint256 pubId)
+        external
+        view
+        returns (uint8)
+    {
+        return _moduleConfigByPubByProfile[profileId][pubId].degreesOfSeparation;
+    }
+
+    /**
      * @dev The data has encoded an array of integers, each integer is a profile ID, the whole array represents a path
      * of n profiles.
      *
@@ -70,23 +203,19 @@ contract DegreesOfSeparationReferenceModule is ModuleBase, IReferenceModule {
      *
      * @param profileId The token ID of the profile associated with the publication being published.
      * @param profileIdPointed The profile ID of the profile associated the publication being referenced.
-     * @param pubIdPointed The publication ID of the publication being referenced.
-     * @param data Encoded data containing the array of profile IDs representing the follower path.
-     *
+     * @param degreesOfSeparation The degrees of separations configured for the given publication.
+     * @param profilePath The array of profile IDs representing the follower path between the owner of the author of the
+     * root publication and the profile authoring the comment.
      */
-    function processComment(
+    function _validateDegreesOfSeparationRestriction(
         uint256 profileId,
         uint256 profileIdPointed,
-        uint256 pubIdPointed,
-        bytes calldata data
-    ) external view override {
-        uint8 degreesOfSeparation = _degreesOfSeparationByPubByProfile[profileIdPointed][
-            pubIdPointed
-        ];
+        uint8 degreesOfSeparation,
+        uint256[] memory profilePath
+    ) internal view {
         if (degreesOfSeparation == 0) {
-            revert CommentsDisabled();
+            revert OpereationDisabled();
         }
-        uint256[] memory profilePath = abi.decode(data, (uint256[]));
         if (profilePath.length == 0 || profilePath.length > degreesOfSeparation) {
             revert InvalidProfilePathLength();
         }
@@ -104,64 +233,10 @@ contract DegreesOfSeparationReferenceModule is ModuleBase, IReferenceModule {
             }
             _checkFollowValidity(profilePath[i], follower);
         }
-        // Checks the last profile in the given path follows the profile authoring the comment.
+        // Checks the last profile in the given path follows the profile performing the comment or mirror.
         // In the previous notation: path[n-1] --> profileId
         follower = IERC721(HUB).ownerOf(profilePath[profilePath.length - 1]);
         _checkFollowValidity(profileId, follower);
-    }
-
-    /**
-     * @notice Processes a mirror action without putting any restriction on it.
-     *
-     * @param profileId The token ID of the profile associated with the publication being published.
-     * @param profileIdPointed The profile ID of the profile associated the publication being referenced.
-     * @param pubIdPointed The publication ID of the publication being referenced.
-     * @param data Arbitrary data.
-     */
-    function processMirror(
-        uint256 profileId,
-        uint256 profileIdPointed,
-        uint256 pubIdPointed,
-        bytes calldata data
-    ) external view override {
-        // Allows anyone to mirror the publication.
-    }
-
-    /**
-     * @notice Sets the degrees of separation for the given publication.
-     *
-     * @param profileId The token ID of the profile publishing the publication.
-     * @param pubId The associated publication's LensHub publication ID.
-     * @param degreesOfSeparation The degrees of separation to set for the given publication.
-     */
-    function setDegreesOfSeparation(
-        uint256 profileId,
-        uint256 pubId,
-        uint8 degreesOfSeparation
-    ) external {
-        // TODO: Struct uint248 + bool; One slot and we have a boolean to flag the publication is being used.abi
-        //       Otherwise we will be emiting an invalid/useless event.
-        if (degreesOfSeparation > MAX_DEGREES_OF_SEPARATION) {
-            revert InvalidDegreesOfSeparation();
-        }
-        _degreesOfSeparationByPubByProfile[profileId][pubId] = degreesOfSeparation;
-        emit DegreesOfSeparationSet(profileId, pubId, degreesOfSeparation);
-    }
-
-    /**
-     * @notice Gets the degrees of separation set for the given publication.
-     *
-     * @param profileId The token ID of the profile publishing the publication.
-     * @param pubId The associated publication's LensHub publication ID.
-     *
-     * @return uint8 The degrees of separation set for the given publication.
-     */
-    function getDegreesOfSeparation(uint256 profileId, uint256 pubId)
-        external
-        view
-        returns (uint8)
-    {
-        return _degreesOfSeparationByPubByProfile[profileId][pubId];
     }
 
     /**
