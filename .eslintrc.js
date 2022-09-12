db966cd0e4e80d3970b3870d05c334d44089dc60
vicnaum <vicnaum@gmail.com>
2022-09-12 15:24:58 +0200

    Fix eslint errors (ts config and turn most into warnings for now)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,39 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  extends: ['standard', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
-    ],
+    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
+    'no-unused-vars': 'warn',
+    'prefer-const': 'warn',
+    eqeqeq: 'warn',
+    camelcase: 'warn',
+    'spaced-comment': 'warn',
+    'node/no-unpublished-import': 'warn',
+    'node/no-unpublished-require': 'warn',
+  },
+  overrides: [
+    {
+      files: ['*.test.ts', '*.spec.ts'],
+      rules: {
+        'no-unused-expressions': 'off',
+      },
+    },
+    {
+      files: ['*.ts'],
+      rules: {
+        'node/no-extraneous-import': 'off',
+      },
+    },
+  ],
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.json', '.node', '.ts', '.d.ts'],
+    },
   },
 };
