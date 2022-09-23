0cb6ce015be4e3f0eb3ee9055b243ae8fc8bd0a1
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-09-23 15:07:37 +0200

    fix: Changed provider to be task param

--- tasks/deployments/collect/deploy-aave-fee-collect-module.ts
+++ tasks/deployments/collect/deploy-aave-fee-collect-module.ts
@@ -9,28 +9,35 @@ export let runtimeHRE: HardhatRuntimeEnvironment;
 const POOL_ADDRESSES_PROVIDER_ADDRESS_MUMBAI = '0x5343b5bA672Ae99d627A1C87866b8E53F47Db2E6';
 const POOL_ADDRESSES_PROVIDER_ADDRESS_POLYGON = '0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb';
 
-task('deploy-auction-collect-module', 'Deploys, verifies and whitelists the auction collect module')
+task(
+  'deploy-aave-fee-collect-module',
+  'Deploys, verifies and whitelists the Aave fee collect module'
+)
   .addParam('hub')
   .addParam('globals')
-  .setAction(async ({ hub, globals }, hre) => {
+  .addParam('poolAddressProvider')
+  .setAction(async ({ hub, globals, poolAddressProvider }, hre) => {
     runtimeHRE = hre;
     const ethers = hre.ethers;
     const accounts = await ethers.getSigners();
     const deployer = accounts[0];
     const governance = accounts[1];
 
+    // Setting pool address provider if left undefined
+    if (!poolAddressProvider)
+      poolAddressProvider =
+        process.env.HARDHAT_NETWORK == 'polygon'
+          ? POOL_ADDRESSES_PROVIDER_ADDRESS_POLYGON
+          : POOL_ADDRESSES_PROVIDER_ADDRESS_MUMBAI;
+
     console.log('\n\n- - - - - - - - Deploying auction collect module\n\n');
-    const auctionCollectModule = await deployWithVerify(
-      new AaveFeeCollectModule__Factory(deployer).deploy(
-        hub,
-        globals,
-        POOL_ADDRESSES_PROVIDER_ADDRESS_MUMBAI
-      ),
-      [hub, globals, POOL_ADDRESSES_PROVIDER_ADDRESS_MUMBAI],
+    const aaveFeeCollectModule = await deployWithVerify(
+      new AaveFeeCollectModule__Factory(deployer).deploy(hub, globals, poolAddressProvider),
+      [hub, globals, poolAddressProvider],
       'contracts/collect/AaveFeeCollectModule.sol:AaveFeeCollectModule'
     );
 
-    console.log('\n\n- - - - - - - - Whitelisting auction collect module\n\n');
+    console.log('\n\n- - - - - - - - Whitelisting Aave fee collect module\n\n');
     // await LensHub__factory.connect(hub, governance).whitelistCollectModule(
     //   auctionCollectModule.address,
     //   true
