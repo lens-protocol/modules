5a25a1b15afa2fb6b37f3ca3a1ec2eb86e842092
vicnaum <vicnaum@gmail.com>
2022-11-17 16:19:15 +0100

    fix: Foundry Deployment Script - add newline in the end of json

--- script/helpers/saveAddress.js
+++ script/helpers/saveAddress.js
@@ -7,5 +7,5 @@ const addresses = require(path.join(__dirname, addressesPath));
 const [targetEnv, contract, address] = process.argv.slice(2);
 addresses[targetEnv][contract] = address;
 
-fs.writeFileSync(path.join(__dirname, addressesPath), JSON.stringify(addresses, null, 2));
+fs.writeFileSync(path.join(__dirname, addressesPath), JSON.stringify(addresses, null, 2) + '\n');
 console.log('Updated `addresses.json`');
