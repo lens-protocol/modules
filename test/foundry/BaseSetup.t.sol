b0e9869d4b665957c025a212accd91e7073d4f59
Victor Naumik <vicnaum@gmail.com>
2022-12-12 18:24:26 +0100

    Update test/foundry/BaseSetup.t.sol
    Co-authored-by: Alan <adonoso@itba.edu.ar>

--- test/foundry/BaseSetup.t.sol
+++ test/foundry/BaseSetup.t.sol
@@ -67,7 +67,7 @@ contract BaseSetup is Test, ForkManagement {
     }
 
     function loadBaseAddresses(string memory json, string memory targetEnv) internal virtual {
-        bytes32 PROXY_IMPLEMENTATION_STORAGE_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
+        bytes32 PROXY_IMPLEMENTATION_STORAGE_SLOT = bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1));
 
         console.log('targetEnv:', targetEnv);
 
